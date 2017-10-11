7ac64db0d47f5ee515bcf830fe46d6cba6d5a63d
ShaToya Ford <codetoya@gmail.com>
2017-10-11 01:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+// write your code below
+var happyHolidays = function () {
+  return "Happy Holidays!"
+};
+
+function happyHolidaysTo (name) {
+  return "Happy Holidays, ${name}!"
+};
+
+function happyHolidayTo (holiday, name) {
+  return "Happy ${holiday, ${name}!"
+};
+
+function holidayCountdown (holiday, days){
+  return "It's ${days} days until ${holiday}!"
+};
+
