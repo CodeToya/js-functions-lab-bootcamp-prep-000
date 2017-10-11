b0e2d75bbc0861783f71272cf9340e7d422c4831
ShaToya Ford <codetoya@gmail.com>
2017-10-11 02:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo (name) {
-  return 'Happy holidays, ${Toya}!'
+  return 'Happy holidays, Toya!'
 }
 
 function happyHolidayTo (holiday,name) {
-  return 'Happy ${Birthday}, ${Bella}!'
+  return 'Happy Birthday, Bella!'
 }
 
 function holidayCountdown (days, holiday){
-  return 'It\'s ${2} days until ${your Birthday}!'
+  return 'It\'s 2 days until your Birthday!'
 }
